2efe72fa66a35a3b994787e254ad2e7069071b89
Zhifeng Gao <zhgao@microsoft.com>
2020-08-17 14:04:41 +0800

    align with benchmark

--- src/benchmark.py
+++ src/benchmark.py
@@ -3,20 +3,49 @@ import numpy as np
 from epiweeks import Week, Year
 import datetime
 import warnings
+import torch
 import os
 warnings.filterwarnings(action='ignore')
 
-def get_benchmark(model_dir, model_name, forecast_date, location2name):
-    df = pd.read_csv(os.path.join(model_dir, model_name,'{}-{}.csv'.format(forecast_date, model_name)))
-    df = df[df.type=='point'][df.target=='1 wk ahead cum death']
-    df['region'] = df.location.map(location2name)
-    df = df[df.region!='US'][['forecast_date','target_end_date','value','region']]
-    df['forecast_date'] = pd.to_datetime(df['forecast_date'])
-    df['target_end_date'] = pd.to_datetime(df['target_end_date'])
+def get_benchmark(model_dir, model_name, location2name, target='1 wk ahead cum death'):
+    baseline_fps = os.listdir(os.path.join(baseline_dir, baseline_name))
+    baseline_fps = [item for item in baseline_fps if not item.startswith('metadata')]
+    if len(baseline_fps)==0:
+        return -1
+    for i,_fp in enumerate(baseline_fps):
+        tmp = pd.read_csv(os.path.join(model_dir, model_name, _fp))
+        tmp = tmp[tmp['type']=='point'][tmp['target']==target]
+        tmp['region'] = tmp.location.map(location2name)
+        tmp = tmp[tmp.region!='US'][['forecast_date','target_end_date','value','region']]
+        tmp['target_start_date'] = pd.to_datetime(tmp['target_end_date']) - datetime.timedelta(days=6)
+        tmp['target_end_date'] = pd.to_datetime(tmp['target_end_date'])
+        if i==0:
+            df = tmp.copy()
+        else:
+            df = pd.concat([df,tmp], axis=0)
+        
+    return df.sort_values(['target_start_date','region']).reset_index(drop=True)
 
-    return df
+def get_label_v2(death_fp):
+    deaths = pd.read_csv(death_fp)
+    ts_features = [item for item in deaths.columns if item not in ['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
+           'Country_Region', 'Lat', 'Long_', 'Combined_Key', 'Population']]
 
-def get_label(death_fp,forecast_date,horizon=7):
+    deaths_us = deaths.groupby('Province_State')[ts_features].sum().diff(axis=1).T
+    deaths_us.index = pd.to_datetime(deaths_us.index)
+    deaths_us['epiweek'] = [Week.fromdate(item) for item in deaths_us.index]
+    deaths_us['forecast_date'] = deaths_us['epiweek'].map(lambda x:x.startdate())
+    deaths_us['target_end_date'] = deaths_us['epiweek'].map(lambda x:x.enddate())
+    deaths_us = deaths_us.set_index(['forecast_date','target_end_date']).stack().reset_index().rename({0:'label','Province_State':'region'},
+                                                                                          axis=1)
+    deaths_us = deaths_us[deaths_us.region!='epiweek']
+    deaths_us = deaths_us.groupby(['forecast_date','target_end_date','region'])['label'].sum().reset_index()
+    deaths_us.loc[deaths_us['label']<0,'label'] = 0
+    deaths_us['cum_label'] = deaths_us.groupby('region')['label'].cumsum()
+    
+    return deaths_us
+
+def get_label(death_fp, target_start_date, horizon=7):
     deaths = pd.read_csv(death_fp)
     ts_features = [item for item in deaths.columns if item not in ['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
            'Country_Region', 'Lat', 'Long_', 'Combined_Key', 'Population']]
@@ -24,18 +53,19 @@ def get_label(death_fp,forecast_date,horizon=7):
     deaths[ts_features] = deaths[ts_features].mask(deaths[ts_features]<0,0)
     deaths_us = deaths.groupby('Province_State')[ts_features].sum().diff(axis=1).T
     deaths_us.index = pd.to_datetime(deaths_us.index)
-    use_index = pd.date_range(forecast_date,freq='D',periods=horizon)
+    use_index = pd.date_range(target_start_date,freq='D',periods=horizon)
     deaths_us = deaths_us.loc[use_index]
-    deaths_us['forecast_date'] = pd.to_datetime(forecast_date)
+    deaths_us['target_start_date'] = pd.to_datetime(use_index[0])
+    deaths_us['target_end_date'] = pd.to_datetime(use_index[-1])
     deaths_us['horizon'] = horizon
-    deaths_us = deaths_us.set_index(['forecast_date','horizon']).stack().reset_index().rename({0:'label','Province_State':'region'},
+    deaths_us = deaths_us.set_index(['target_start_date','target_end_date','horizon']).stack().reset_index().rename({0:'label','Province_State':'region'},
                                                                                           axis=1)
-    deaths_us = deaths_us.groupby(['forecast_date','horizon','region'])['label'].sum().reset_index()
+    deaths_us = deaths_us.groupby(['target_start_date','horizon','region'])['label'].sum().reset_index()
     deaths_us.loc[deaths_us['label']<0,'label'] = 0
 
     deaths_cum =  deaths.groupby('Province_State')[ts_features].sum().T
     deaths_cum.index = pd.to_datetime(deaths_cum.index)
-    deaths_cum = deaths_cum.loc[pd.to_datetime(forecast_date)- datetime.timedelta(days=1)].reset_index()
+    deaths_cum = deaths_cum.loc[pd.to_datetime(target_start_date)- datetime.timedelta(days=1)].reset_index()
     deaths_cum.columns = ['region','cum_label']
     deaths_us = pd.merge(deaths_us,deaths_cum,how='left',on=['region'])
     deaths_us['cum_label'] = deaths_us['cum_label'] + deaths_us['label']
@@ -54,19 +84,21 @@ def get_model_predict(model_fp):
 if __name__ == "__main__":
     # benchmark link: https://github.com/reichlab/covid19-forecast-hub
     baseline_dir = '/home/zhgao/COVID-Research/covid19-forecast-hub/data-processed'
-    baseline_name = 'UT-Mobility'
-    forecast_date = '2020-06-08'
+    baseline_name = 'UCLA-SuEIR'
     location_fp = '/home/zhgao/COVID-Research/covid19-forecast-hub/data-locations/locations.csv'
     death_fp ='/home/zhgao/COVID19/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
     location = pd.read_csv(location_fp)
     location2name = dict(zip(location['location'],location['location_name']))   
 
-    model_fp = '/home/zhgao/COVID-Research/US_sandwich_7_6_29'
+    
+    # sunday date of a epidemiological week
+    target_start_date = '2020-06-28'
+    model_fp = '/home/zhgao/COVID-Research/US_sandwich_7_{}'.format('_'.join(target_start_date.split('-')[-2:]))
     res_test = get_model_predict(model_fp)
 
-    gt = get_label(death_fp, forecast_date, horizon=7)
-    pred = get_benchmark(baseline_dir, baseline_name, forecast_date, location2name) 
-    pred = pd.merge(pred, gt, on=['forecast_date','region'], how='left') 
+    gt = get_label(death_fp, target_start_date, horizon=7)
+    pred = get_benchmark(baseline_dir, baseline_name, location2name) 
+    pred = pd.merge(gt, pred, on=['target_start_date','region'], how='inner')
     print("{}_MAE: ".format(baseline_name), np.abs(pred['value'] - pred['cum_label']).mean())
     print("MAE: ", np.abs(res_test['pred'] - res_test['label']).mean())
     print(pred.head())
--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_6_27 --forecast_date 2020-06-27 --label deaths_target
-CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_6_22 --forecast_date 2020-06-22 --label deaths_target
-CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_6_15 --forecast_date 2020-06-15 --label deaths_target
-CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_6_08 --forecast_date 2020-06-08 --label deaths_target
-CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_6_07 --forecast_date 2020-06-07 --label deaths_target
+CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_06_07 --forecast_date 2020-06-07 --label deaths_target
+CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_06_14 --forecast_date 2020-06-14 --label deaths_target
+CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_06_21 --forecast_date 2020-06-21 --label deaths_target
+CUDA_VISIBLE_DEVICES=0 python main_task.py --model_type sandwich --data_fp ../data/daily_mobility_US_7.csv --exp_dir ../US_sandwich_7_06_28 --forecast_date 2020-06-28 --label deaths_target
